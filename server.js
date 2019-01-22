3ca8fccfec9018a5e77fa3e854db8b6e7c69a225
DaanVerschraegen <daan.verschraegen@outlook.com>
2019-01-22 14:06:45 +0100

    server.js aangepast, search werkt

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ var db;
 MongoClient.connect('mongodb://localhost:27017/examen', { useNewUrlParser: true }, (err, database) => {
   if (err) return console.log(err)
   db = database.db('examen')
+  db.createCollection('inhaal',
+	function(err,res){
+	  if (err) return console.log(err)
+  	})
   app.listen(process.env.PORT || 3000, () => {
     console.log('Listening on port 3000')
   })
@@ -37,25 +41,17 @@ app.post('/add', (req, res) => {
 
 // Show the search form
 app.get('/search', (req, res) => {
-   res.render('search.ejs', { artist: '' })
+   res.render('search.ejs', {})
 })
 
 // Find a product
 app.post('/search', (req, res) => {
  var query = { name: req.body.name }
- db.collection('artists').find(query).toArray(function(err, result) {
+ db.collection('inhaal').find(query).toArray(function(err, result) {
    if (err) return console.log(err)
    if (result == '')
        res.render('search_not_found.ejs', {})
    else
        res.render('search_result.ejs', { artist: result[0] })
  });
-})
-
-// Delete a product
-app.post('/delete', (req, res) => {
-  db.collection('artists').findOneAndDelete({ name: req.body.name }, (err, result) => {
-    if (err) return res.send(500, err)
-    res.redirect('/list')
-  })
 })
